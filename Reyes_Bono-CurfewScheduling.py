94805a1f5793b0cc85eacacd92ac1166652d233a
wth is a pull request <117620492+PalaboiCarti@users.noreply.github.com>
2023-10-15 19:27:28 +0800

    🚔 ErrorHandlers, Baybeh
    bawal ka mag error tanga kaya sa code ko chinecheck TALAGA

--- Reyes_Bono-CurfewScheduling.py
+++ Reyes_Bono-CurfewScheduling.py
@@ -1,5 +1,5 @@
-#oct/15/2023 06:45
-#Sixth Commit 666 gang gang
+#oct/15/2023 07:26
+#Seventh Commit owyeah
 
 districts = ["District 1", "District 2", "District 3", "District 4", "District 5", "District 6", "District 7", "District 8", "District 9"]
 
@@ -7,20 +7,72 @@ blue_marketday = "MarketDays : Tue, Sat. 06:00 - 19:00"
 blue_curfew = "Curfew: Mon, Tue, Fri. 10:00 - 05:00"
 red_marketday = "MarketDays : Mon, Wed. 06:00 - 21:00"
 
+def get_name():
+    while True:
+        print("Please follow this format: ")
+        print("<LastName>, <FirstName(s)> <MiddleInitial>.")
+        name = input("Enter your name: ")
+        if all(char.isalpha() or char.isspace() or char in "., " for char in name):
+            print("---------------------")
+            print(f"Entered name: {name}")
+            while True:
+                confirmation = input("Please confirm if the credentials are correct (y/n): ")
+                if confirmation == "y":
+                    print("---------------------")
+                    return name
+                elif confirmation == "n":
+                    print("Resubmit credentials.")
+                    print("---------------------")
+                    break  
+                else:
+                    print("Invalid response. Please enter 'y' or 'n'.")
+                    print("---------------------")
+        else:
+            print("Error: Invalid credentials. Please try again.")
+            print("---------------------")
+
+
 def get_age():
     while True:
         try:
             age = int(input("Enter age: "))
-            return age
+            while True:
+                print("---------------------")
+                print(f"Entered age: {age}")
+                confirmation = input("Please confirm if the credentials are correct (y/n): ")
+                if confirmation == "y":
+                    print("---------------------")
+                    return age
+                elif confirmation == "n":
+                    print("Resubmit credentials.")
+                    print("---------------------")
+                    break  
+                else:
+                    print("Invalid response. Please enter 'y' or 'n'.")
+                    print("---------------------")
         except ValueError:
             print("Invalid input")
+            print("---------------------")
             
 #get district 
 def get_district():
     while True:
         district = input("Enter District: ")
         if district in districts:
-            return district
+            while True:
+                print("---------------------")
+                print(f"Entered District: {district}")
+                confirmation = input("Please confirm if the credentials are correct (y/n): ")
+                if confirmation == "y":
+                    print("---------------------")
+                    return district
+                elif confirmation == "n":
+                    print("Resubmit credentials.")
+                    print("---------------------")
+                    break  
+                else:
+                    print("Invalid response. Please enter 'y' or 'n'.")
+                    print("---------------------")
         else:
             print("Error : Unidentified district.")
             print("Please select one of the following districts:")
@@ -43,11 +95,13 @@ def get_schedule(age, district):
             print("Error: Invalid credentials. Please enter a registered district.")
 
 def display_profile():
+    name = get_name()
     age = get_age()
     district = get_district()
     schedule = get_schedule(age, district)
     
-    print(f"==> I am {age} years old.")
+    print(f"NAME : {name}")
+    print(f"AGE : {age}")
     
     if isinstance(schedule, tuple) and blue_marketday in schedule and blue_curfew in schedule:
         print(f"==> Schedules:")
