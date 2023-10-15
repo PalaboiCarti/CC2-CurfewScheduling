efc63112d517d6a6d7011a53b514a284d539616b
wth is a pull request <117620492+PalaboiCarti@users.noreply.github.com>
2023-10-15 17:46:41 +0800

    🚔 Simplified this mf
    separated the functions for inputting district code and schedule generator

--- Reyes_Bono-CurfewScheduling.py
+++ Reyes_Bono-CurfewScheduling.py
@@ -1,5 +1,5 @@
-#oct/15/2023 05:18
-#FOURTH COMMIT MOTHERFUCKER WAHAHAHAHAHAHHA HELL YEAH
+#oct/15/2023 05:44
+#Fifth commit mahnegggga
 
 districts = ["District 1", "District 2", "District 3", "District 4", "District 5", "District 6", "District 7", "District 8", "District 9"]
 
@@ -10,17 +10,27 @@ def get_age():
             return age
         except ValueError:
             print("Invalid input")
+            
+#get district 
+def get_district():
+    while True:
+        district = input("Enter District: ")
+        if district in districts:
+            print("Registered district")
+        else:
+            print("Unregistered district")
+        return district
   
 #curfew generator
-def get_district(age):
+def get_schedule(age, district):
     blue = [districts[0], districts[2], districts[4]]
     red = [districts[1], districts[3], districts[5]]
     green = [districts[6], districts[7], districts[8]]
     
-    blue_marketday = "MarketDay : Tue, Sat. 06:00 - 19:00"
+    blue_marketday = "MarketDays : Tue, Sat. 06:00 - 19:00"
     blue_curfew = "Curfew: Mon, Tue, Fri. 10:00 - 05:00"
+    red_marketday = "MarketDays : Mon, Wed. 06:00 - 21:00"
     while True:
-        district = input("Enter district: ")
         if district in blue:
             if age > 60 or age < 18:
                 return blue_marketday, blue_curfew
@@ -31,12 +41,14 @@ def get_district(age):
 
 def display_profile():
     age = get_age()
-    district = get_district(age)
+    district = get_district()
+    schedule = get_schedule(age, district)
 
     print(f"==> I am {age} years old.")
-    print(f"==> Schedules: {district}.")
+    print(f"==> Schedules: {schedule}.")
 
 display_profile()
+
 
 
 
